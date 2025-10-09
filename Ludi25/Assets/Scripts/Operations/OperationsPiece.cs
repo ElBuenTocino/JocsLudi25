6e7e2d9cc09ddfeea7a7865c8536fbd83236e3c8
ElBuenTocino <paumm8@gmail.com>
2025-10-09 21:52:50 +0200

    coses sense importancia

--- Ludi25/Assets/Scripts/Operations/OperationsPiece.cs
+++ Ludi25/Assets/Scripts/Operations/OperationsPiece.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
+using TMPro;
+using UnityEditor.ShaderGraph.Internal;
 
 public class OperationsPiece : MonoBehaviour
 {
     public int value;
-
+    public TMP_Text number;
+    public enum Type
+    {
+        num1, num2, sign, answer
+    }
+    public Type type;
     void Start()
     {
-        
+        number = GetComponentInChildren<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (type != Type.sign)
+        {
+            number.text = value.ToString();
+        }   
     }
 }
