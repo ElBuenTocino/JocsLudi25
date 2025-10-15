db912a7784bcf3c6dc78573a74456b8f4c1fa5da
ElBuenTocino <paumm8@gmail.com>
2025-10-15 09:46:30 +0200

    Animals Revamp
    Dos tirachinas, animals es mouen horitzontalment i velocitats random

--- Ludi25/Assets/Scripts/Animals/AnimalManager.cs
+++ Ludi25/Assets/Scripts/Animals/AnimalManager.cs
@@ -6,7 +6,7 @@ public class AnimalManager : MonoBehaviour
 {
     public float timeToSpawn = 2f;
     public GameObject animalPrefab;
-    public Transform ySpawn, minX, maxX;
+    public Transform minY, maxY, minX, maxX;
 
     public int score = 0, maxScore;
     public TextMeshProUGUI scoreText;
@@ -34,15 +34,22 @@ public class AnimalManager : MonoBehaviour
         if (timer >= timeToSpawn)
         {
             timer = 0f;
-            float x = Random.Range(minX.position.x, maxX.position.x);
-            float y = ySpawn.position.y;
+            float y = Random.Range(minY.position.y, maxY.position.y);
+            float x = Random.Range(0, 2);
+            if (x == 1) { x = minX.position.x; }
+            else { x = maxX.position.x; }
             GameObject newAnimal = Instantiate(animalPrefab, new Vector2(x, y), Quaternion.identity);
             newAnimal.GetComponent<AnimalBehaviour>().manager = this;
+            if (newAnimal.transform.position.x == maxX.position.x)
+            {
+                newAnimal.GetComponent<AnimalBehaviour>().speed *= -1;
+            }
         }
     }
 
     void Win()
     {
+        GetComponent<SceneHandler>().ChangeScene();
         Debug.Log("Won!");
     }
 }
