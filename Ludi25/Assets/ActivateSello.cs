8176fb3628bc6c441ea352a07392f7f5c2e3b2b9
ElBuenTocino <paumm8@gmail.com>
2025-10-27 23:40:34 +0100

    calendari

--- Ludi25/Assets/ActivateSello.cs
+++ Ludi25/Assets/ActivateSello.cs
@@ -9,6 +9,12 @@ public class ActivateSello : MonoBehaviour
         if (PlayerPrefs.GetInt("WinMates") == 1 && PlayerPrefs.GetInt("WinWords") == 1 && PlayerPrefs.GetInt("WinMedi") == 1)
         {
             transform.GetChild(0).gameObject.SetActive(true);
+            transform.GetChild(1).gameObject.SetActive(false);
+        }
+        else
+        {
+            transform.GetChild(1).gameObject.SetActive(true);
+            transform.GetChild(0).gameObject.SetActive(false);
         }
     }
 }
